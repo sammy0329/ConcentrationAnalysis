7eb11a4ac6f1f00ba86718176a837a4a9ffa3428
june <jhjun1030@gmail.com>
2022-05-08 22:58:51 +0900

    5

--- db_auth.py
+++ db_auth.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-key_path = "resource/db_auth_key.json"
-db_url = "https://test-db-b261c-default-rtdb.firebaseio.com/"
-cred = credentials.Certificate(key_path)
-firebase_admin.initialize_app(cred, {'databaseURL' : db_url})
-dir = db.reference()
+import os
+print(os.getcwd())
+
+# key_path = "resource/db_auth_key.json"
+# db_url = "https://test-db-b261c-default-rtdb.firebaseio.com/"
+# cred = credentials.Certificate(key_path)
+# firebase_admin.initialize_app(cred, {'databaseURL' : db_url})
+# dir = db.reference()
